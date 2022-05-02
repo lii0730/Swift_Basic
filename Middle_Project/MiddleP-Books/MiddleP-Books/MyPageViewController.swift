c55fae1fcd1a0d7c3679482089e4f6ec4d382d35
LeeHsss <lii0730@icloud.com>
2022-05-03 00:24:55 +0900

    마이페이지 레이아웃 작업

--- Middle_Project/MiddleP-Books/MiddleP-Books/MyPageViewController.swift
+++ Middle_Project/MiddleP-Books/MiddleP-Books/MyPageViewController.swift
@@ -20,8 +20,15 @@ class MyPageViewController: UIViewController {
         return label
     }()
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     func layoutSubView() {
-        [emptyTextLabel].forEach {
+        [emptyTextLabel, tableView].forEach {
             view.addSubview($0)
         }
         
@@ -29,22 +36,64 @@ class MyPageViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.centerY.equalToSuperview()
         }
+        
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.top.bottom.equalToSuperview()
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        layoutSubView()
-        
         if let bookList = self.bookList {
             if bookList.isEmpty {
                 self.emptyTextLabel.isHidden = false
+                self.tableView.isHidden = true
             } else {
                 self.emptyTextLabel.isHidden = true
+                self.tableView.isHidden = false
             }
         } else {
-            NSLog("Error: Optional UnWrapping, bookList")
+            NSLog("Error: Optional Not UnWrapping, bookList")
         }
+        
+        layoutSubView()
+        
+        tableView.register(SelectedBookCell.self, forCellReuseIdentifier: "SelectedBookCell")
+    }
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let bookList = self.bookList else { return 0 }
+        
+        return bookList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SelectedBookCell") as? SelectedBookCell else {
+            return UITableViewCell()
+        }
+        
+        guard let bookList = self.bookList else { return UITableViewCell() }
+        let book = bookList[indexPath.row]
+        cell.bookImageView.image = UIImage(named: book.imageURL)
+        cell.titleLabel.text = book.title
+        cell.deleteButton.addTarget(self, action: #selector(OnClick), for: .touchUpInside)
+        
+        return cell
+    }
+}
+
+extension MyPageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+extension MyPageViewController {
+    @objc func OnClick() {
+        
     }
 }
